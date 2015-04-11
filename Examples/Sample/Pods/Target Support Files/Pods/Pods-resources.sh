52956193e403e97aa5d9301751e728f0aa65aa43
dzenbot <iromero@dzen.cl>
2015-04-11 14:42:03 -0300

    Updates sample's pod files

--- Examples/Sample/Pods/Target Support Files/Pods/Pods-resources.sh
+++ Examples/Sample/Pods/Target Support Files/Pods/Pods-resources.sh
@@ -6,6 +6,8 @@ mkdir -p "${CONFIGURATION_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}"
 RESOURCES_TO_COPY=${PODS_ROOT}/resources-to-copy-${TARGETNAME}.txt
 > "$RESOURCES_TO_COPY"
 
+XCASSET_FILES=""
+
 install_resource()
 {
   case $1 in
@@ -36,6 +38,7 @@ install_resource()
       xcrun mapc "${PODS_ROOT}/$1" "${CONFIGURATION_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}/`basename "$1" .xcmappingmodel`.cdm"
       ;;
     *.xcassets)
+      XCASSET_FILES="$XCASSET_FILES '${PODS_ROOT}/$1'"
       ;;
     /*)
       echo "$1"
@@ -47,41 +50,72 @@ install_resource()
       ;;
   esac
 }
-          install_resource "../../../Source/Resources/dzn_icn_activity_chrome@2x.png"
-                    install_resource "../../../Source/Resources/dzn_icn_activity_chrome@2x~ipad.png"
-                    install_resource "../../../Source/Resources/dzn_icn_activity_dolphin@2x.png"
-                    install_resource "../../../Source/Resources/dzn_icn_activity_dolphin@2x~ipad.png"
-                    install_resource "../../../Source/Resources/dzn_icn_activity_link@2x.png"
-                    install_resource "../../../Source/Resources/dzn_icn_activity_link@2x~ipad.png"
-                    install_resource "../../../Source/Resources/dzn_icn_activity_opera@2x.png"
-                    install_resource "../../../Source/Resources/dzn_icn_activity_opera@2x~ipad.png"
-                    install_resource "../../../Source/Resources/dzn_icn_activity_safari@2x~ipad.png"
-                    install_resource "../../../Source/Resources/dzn_icn_toolbar_action.png"
-                    install_resource "../../../Source/Resources/dzn_icn_toolbar_action@2x.png"
-                    install_resource "../../../Source/Resources/dzn_icn_toolbar_action@3x.png"
-                    install_resource "../../../Source/Resources/dzn_icn_toolbar_backward.png"
-                    install_resource "../../../Source/Resources/dzn_icn_toolbar_backward@2x.png"
-                    install_resource "../../../Source/Resources/dzn_icn_toolbar_backward@3x.png"
-                    install_resource "../../../Source/Resources/dzn_icn_toolbar_forward.png"
-                    install_resource "../../../Source/Resources/dzn_icn_toolbar_forward@2x.png"
-                    install_resource "../../../Source/Resources/dzn_icn_toolbar_forward@3x.png"
-                    install_resource "../../../Source/Resources/dzn_icn_toolbar_reload.png"
-                    install_resource "../../../Source/Resources/dzn_icn_toolbar_reload@2x.png"
-                    install_resource "../../../Source/Resources/dzn_icn_toolbar_reload@3x.png"
-                    install_resource "../../../Source/Resources/dzn_icn_toolbar_stop.png"
-                    install_resource "../../../Source/Resources/dzn_icn_toolbar_stop@2x.png"
-                    install_resource "../../../Source/Resources/dzn_icn_toolbar_stop@3x.png"
-                    install_resource "../../../Source/Resources/de.lproj"
-                    install_resource "../../../Source/Resources/en.lproj"
-                    install_resource "../../../Source/Resources/es.lproj"
-          
+if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_resource "../../../Source/Resources/dzn_icn_activity_chrome@2x.png"
+  install_resource "../../../Source/Resources/dzn_icn_activity_chrome@2x~ipad.png"
+  install_resource "../../../Source/Resources/dzn_icn_activity_dolphin@2x.png"
+  install_resource "../../../Source/Resources/dzn_icn_activity_dolphin@2x~ipad.png"
+  install_resource "../../../Source/Resources/dzn_icn_activity_link@2x.png"
+  install_resource "../../../Source/Resources/dzn_icn_activity_link@2x~ipad.png"
+  install_resource "../../../Source/Resources/dzn_icn_activity_opera@2x.png"
+  install_resource "../../../Source/Resources/dzn_icn_activity_opera@2x~ipad.png"
+  install_resource "../../../Source/Resources/dzn_icn_activity_safari@2x~ipad.png"
+  install_resource "../../../Source/Resources/dzn_icn_toolbar_action.png"
+  install_resource "../../../Source/Resources/dzn_icn_toolbar_action@2x.png"
+  install_resource "../../../Source/Resources/dzn_icn_toolbar_action@3x.png"
+  install_resource "../../../Source/Resources/dzn_icn_toolbar_backward.png"
+  install_resource "../../../Source/Resources/dzn_icn_toolbar_backward@2x.png"
+  install_resource "../../../Source/Resources/dzn_icn_toolbar_backward@3x.png"
+  install_resource "../../../Source/Resources/dzn_icn_toolbar_forward.png"
+  install_resource "../../../Source/Resources/dzn_icn_toolbar_forward@2x.png"
+  install_resource "../../../Source/Resources/dzn_icn_toolbar_forward@3x.png"
+  install_resource "../../../Source/Resources/dzn_icn_toolbar_reload.png"
+  install_resource "../../../Source/Resources/dzn_icn_toolbar_reload@2x.png"
+  install_resource "../../../Source/Resources/dzn_icn_toolbar_reload@3x.png"
+  install_resource "../../../Source/Resources/dzn_icn_toolbar_stop.png"
+  install_resource "../../../Source/Resources/dzn_icn_toolbar_stop@2x.png"
+  install_resource "../../../Source/Resources/dzn_icn_toolbar_stop@3x.png"
+  install_resource "../../../Source/Resources/de.lproj"
+  install_resource "../../../Source/Resources/en.lproj"
+  install_resource "../../../Source/Resources/es.lproj"
+fi
+if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_resource "../../../Source/Resources/dzn_icn_activity_chrome@2x.png"
+  install_resource "../../../Source/Resources/dzn_icn_activity_chrome@2x~ipad.png"
+  install_resource "../../../Source/Resources/dzn_icn_activity_dolphin@2x.png"
+  install_resource "../../../Source/Resources/dzn_icn_activity_dolphin@2x~ipad.png"
+  install_resource "../../../Source/Resources/dzn_icn_activity_link@2x.png"
+  install_resource "../../../Source/Resources/dzn_icn_activity_link@2x~ipad.png"
+  install_resource "../../../Source/Resources/dzn_icn_activity_opera@2x.png"
+  install_resource "../../../Source/Resources/dzn_icn_activity_opera@2x~ipad.png"
+  install_resource "../../../Source/Resources/dzn_icn_activity_safari@2x~ipad.png"
+  install_resource "../../../Source/Resources/dzn_icn_toolbar_action.png"
+  install_resource "../../../Source/Resources/dzn_icn_toolbar_action@2x.png"
+  install_resource "../../../Source/Resources/dzn_icn_toolbar_action@3x.png"
+  install_resource "../../../Source/Resources/dzn_icn_toolbar_backward.png"
+  install_resource "../../../Source/Resources/dzn_icn_toolbar_backward@2x.png"
+  install_resource "../../../Source/Resources/dzn_icn_toolbar_backward@3x.png"
+  install_resource "../../../Source/Resources/dzn_icn_toolbar_forward.png"
+  install_resource "../../../Source/Resources/dzn_icn_toolbar_forward@2x.png"
+  install_resource "../../../Source/Resources/dzn_icn_toolbar_forward@3x.png"
+  install_resource "../../../Source/Resources/dzn_icn_toolbar_reload.png"
+  install_resource "../../../Source/Resources/dzn_icn_toolbar_reload@2x.png"
+  install_resource "../../../Source/Resources/dzn_icn_toolbar_reload@3x.png"
+  install_resource "../../../Source/Resources/dzn_icn_toolbar_stop.png"
+  install_resource "../../../Source/Resources/dzn_icn_toolbar_stop@2x.png"
+  install_resource "../../../Source/Resources/dzn_icn_toolbar_stop@3x.png"
+  install_resource "../../../Source/Resources/de.lproj"
+  install_resource "../../../Source/Resources/en.lproj"
+  install_resource "../../../Source/Resources/es.lproj"
+fi
+
 rsync -avr --copy-links --no-relative --exclude '*/.svn/*' --files-from="$RESOURCES_TO_COPY" / "${CONFIGURATION_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}"
 if [[ "${ACTION}" == "install" ]]; then
   rsync -avr --copy-links --no-relative --exclude '*/.svn/*' --files-from="$RESOURCES_TO_COPY" / "${INSTALL_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}"
 fi
 rm -f "$RESOURCES_TO_COPY"
 
-if [[ -n "${WRAPPER_EXTENSION}" ]] && [ "`xcrun --find actool`" ] && [ `find . -name '*.xcassets' | wc -l` -ne 0 ]
+if [[ -n "${WRAPPER_EXTENSION}" ]] && [ "`xcrun --find actool`" ] && [ -n "$XCASSET_FILES" ]
 then
   case "${TARGETED_DEVICE_FAMILY}" in
     1,2)
@@ -97,5 +131,6 @@ then
       TARGET_DEVICE_ARGS="--target-device mac"
       ;;
   esac
-  find "${PWD}" -name "*.xcassets" -print0 | xargs -0 actool --output-format human-readable-text --notices --warnings --platform "${PLATFORM_NAME}" --minimum-deployment-target "${IPHONEOS_DEPLOYMENT_TARGET}" ${TARGET_DEVICE_ARGS} --compress-pngs --compile "${BUILT_PRODUCTS_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}"
+  while read line; do XCASSET_FILES="$XCASSET_FILES '$line'"; done <<<$(find "$PWD" -name "*.xcassets" | egrep -v "^$PODS_ROOT")
+  echo $XCASSET_FILES | xargs actool --output-format human-readable-text --notices --warnings --platform "${PLATFORM_NAME}" --minimum-deployment-target "${IPHONEOS_DEPLOYMENT_TARGET}" ${TARGET_DEVICE_ARGS} --compress-pngs --compile "${BUILT_PRODUCTS_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}"
 fi
